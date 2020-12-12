d00e11dbc64a939146abd3051f2cca27ae716ddb
Saulo <sauloaguiar89@gmail.com>
2020-12-12 15:49:51 -0300

    # [Commit Title - Summary of changes] Initial project structure with initial stopwatch machine

---
+++ src/Stopwatch.js
@@ -0,0 +1,66 @@
+import {
+  Machine,
+  State,
+  actions,
+  assign,
+  send,
+  sendParent,
+  interpret,
+  spawn
+} from 'xstate';
+import { useMachine } from '@xstate/react';
+
+const stopwatchMachine = Machine({
+  id: 'stopwatch',
+  initial: 'idle',
+  context: {
+    laps: 0
+  },
+  states: {
+    idle: {
+      on: {
+        START: 'running'
+      }
+    },
+    paused: {
+      on: {
+        START: 'running',
+        RESET: 'idle'
+      }
+    },
+    running: {
+      on: {
+        RESET:'idle',
+        PAUSE:'paused',
+        LAP: {
+          target: 'running',
+          actions: assign({
+            laps: (context, event) => context.laps+1
+            })
+          }
+        }
+      }
+    },
+});
+
+const Stopwatch = () => {
+  const [current, send] = useMachine(stopwatchMachine);
+  
+  return (
+    <div>
+      <div>Current state: {current.value}</div>
+      <div>Lap count: {current.context.laps}</div>
+      <div>
+        <button disabled={current.matches('IDLE')} onClick={() => {
+          current.matches('running') ? send('PAUSE') : send('START')
+          }
+        }>
+          {current.matches('running') ? 'PAUSE' : 'START'}</button>
+        <button onClick={() => send('RESET')}>RESET</button>
+        <button onClick={() => send('LAP')}>LAP</button>
+      </div>
+    </div>
+  )
+}
+
+export default Stopwatch;
