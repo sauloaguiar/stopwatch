21188dbbc86169e3d83b1157730b9bf40e67bd89
Saulo <sauloaguiar89@gmail.com>
2020-12-14 20:48:37 -0300

    Moves reset logic to entry prop of idle state Adds ticker function to count ticks Uses invoke prop to run the ticker
    not erasing laps as it gets reset

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -10,57 +10,84 @@ import {
 } from 'xstate';
 import { useMachine } from '@xstate/react';
 
+const ticker = ctx => cb => {
+  console.log( 'ticker invoked', ctx);
+  const interval = setInterval(() => {
+    cb("TICK");
+    console.log('tick');
+  }, ctx.interval * 1000);
+  return () => clearInterval(interval);
+};
+
 const stopwatchMachine = Machine({
   id: 'stopwatch',
   initial: 'idle',
   context: {
-    laps: 0
+    laps: [],
+    interval: 1,
+    elapsed: 0
   },
   states: {
     idle: {
       on: {
         START: 'running'
-      }
+      },
+      entry: 
+        assign({
+          laps: [],
+          elapsed: 0
+        })
     },
     paused: {
       on: {
         START: 'running',
-        RESET: {
-          target:'idle',
-          actions: assign({
-            laps: (context, _) => context.laps = 0
-          })
-        }
+        RESET: 'idle'
       }
-
     },
     running: {
+      invoke: {
+        src: ticker,
+        id: 'ticker',
+        onDone: () => console.log('done was called')
+      },
       on: {
-        RESET:{
-          target:'idle',
-          actions: assign({
-            laps: (context, _) => context.laps = 0
-          })
-        },
+        RESET:'idle',
         PAUSE:'paused',
         LAP: {
           target: 'running',
           actions: assign({
-            laps: (context, event) => context.laps+1
+            laps: (context, event) => {
+              console.log('elapsed: ', context.elapsed);
+              context.laps.push(context.elapsed)
+              return context.laps;
+            }
             })
-          }
-        }
+          },
+        TICK: {
+          actions: assign({
+            elapsed: (ctx) => ctx.elapsed + ctx.interval,
+          }),
+        },
+      },
       }
     },
 });
 
 const Stopwatch = () => {
   const [current, send] = useMachine(stopwatchMachine);
-  
+
   return (
     <div>
-      <div>Current state: {current.value}</div>
-      <div>Lap count: {current.context.laps}</div>
+      <div>Current state: {JSON.stringify(current.value)}</div>
+      <div>Lap count: {current.context.laps.length}</div>
+      {current.context.laps.length > 0 &&
+        <div>
+          {current.context.laps.map(lap => (
+            <ul key={lap}>lap {lap}</ul>
+          ))}
+        </div>
+      }
+      <div>Elapsed: {current.context.elapsed}</div>
       <div>
         <button disabled={current.matches('IDLE')} onClick={() => {
           current.matches('running') ? send('PAUSE') : send('START')
